b99f7b201093eaaeab6ea34a1e648eac99444b9f
karim <karim.elmougi@gmail.com>
2021-04-17 16:27:35 -0400

    Fun stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![feature(asm)]
 #![deny(warnings)]
 
 fn main() {
@@ -12,19 +13,50 @@ fn main() {
 // **NOTE**
 // - do NOT change any existing codes except that `todo!()`
 //
+
+// The *sane* way to do this would be to take a mut reference to i as argument 
+// and set it to 1. But, given the above constraints, we have to turn to
+// diabolical insanity like this.
+//
 fn change_value() {
-    todo!()
+    // I am not even going to pretend this code works universally, and to drive
+    // home this fact I am going to use inline x86 assembly. This is, on the
+    // whole, a terrible idea.
+    //
+    // It works on my particular configuration of CPU (x86_64), 
+    // OS (Linux 4.14.226-170.lts), and rustc version (latest nightly), at this
+    // particular point in time. It probably does not work with any other 
+    // configuration, and may depend on the alignment of Saturn with Andromeda 
+    // for all I know.
+    let ptr_to_i: *mut i64;
+    unsafe{
+        asm!(
+            "lea {}, [rsp + 8]",
+            out(reg) ptr_to_i
+            );
+    }
+
+    unsafe { *ptr_to_i = 1; }
+
+    // Since this is all undefined behavior, the optimization passes of rustc
+    // and/or LLVM will optimize this code away unless we cause some kind of
+    // side effect, by printing to console in this case.
+    println!("{:p}", ptr_to_i);
 }
 
 #[cfg(test)]
 mod test {
+    use futures::executor::block_on;
+
     #[test]
     fn test1() {
         let mut a = Vec::new();
 
         {
             // fix this line to make this test pass
-            a[10000000] = 1;
+            let i = 10000000;
+            a.resize(i+1, 0);
+            a[i] = 1;
         }
 
         assert_eq!(a[10000000], 1);
@@ -38,7 +70,7 @@ mod test {
 
         {
             // fix this line to make this test pass
-            b = a();
+            b = block_on(a);
         }
 
         assert_eq!(b, "Hello World!");
